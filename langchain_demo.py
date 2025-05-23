17e7aab43891c709942451da1b9d83040b4ff0f9
María <msachezcarrasco@gmail.com>
2025-05-23 13:13:40 +0200

    Data process and Modelfiles

--- langchain_demo.py
+++ langchain_demo.py
@@ -25,9 +25,13 @@ examples = [
         "output": "El que quiera saber\nde qué color son las penas\nsiente plaza de soldado\ny auséntese de su tierra"
     },
     {
-        "input": "Me dices que era un gato el que entró por tu ventana. En la vida he visto yo, serrano de mi querer, un gato negro y con sotana.",
-        "output": "Me dices que era un gato\nel que entró por tu ventana.\nEn la vida he visto yo,\nserrano de mi querer,\nun gato negro y con sotana."
+        "input": "Ni la fuente más risueña ni el canario más sonoro ni la tórtola en su breña cantarán como yo lloro gotas de sangre por ella",
+        "output": "Ni la fuente más risueña\nni el canario más sonoro\nni la tórtola en su breña\ncantarán como yo lloro\ngotas de sangre por ella"
     }
+    # {
+    #     "input": "Me dices que era un gato el que entró por tu ventana. En la vida he visto yo, serrano de mi querer, un gato negro y con sotana.",
+    #     "output": "Me dices que era un gato\nel que entró por tu ventana.\nEn la vida he visto yo,\nserrano de mi querer,\nun gato negro y con sotana."
+    # }
 ]
 
 example_prompt = PromptTemplate(
@@ -73,25 +77,32 @@ few_shot_prompt = FewShotPromptTemplate(
 #llm = OllamaLLM(model="segmenter2_llama2", params={"format": "json"})
 #llm = OllamaLLM(model="segmenter2_gemma3", params={"format": "json"})
 #llm = OllamaLLM(model="segmenter3_deepseekr1", params={"format": "json"})
-llm = OllamaLLM(model="segmenterft_llama2", params={"format": "json"})
+llm = OllamaLLM(model="segmenterstablelm2", params={"format": "json"})
 
 
 #input_song = "Que rueden, que rueden, las cáscaras de huevo: las lavanderas hacen así, las planchadoras hacen así, las barrenderas hacen así"
-#input_song = "A un campo lejos me fui por ver si así te olvidaba y mientras más lejos me iba, mucho más te recordaba. Al darte pestañas negras, Dios, sin duda, se propuso, que por las muertes que causas, ¡ay!, Soleá, Soleá, tus ojitos vistan luto. Dicen que ya no hay locura y yo digo que es verdad, pues si locura aún hubiera, loco estuviera yo ya. Entre la tierra y er sielo no hay mugeres con mas sal que las mugeres de España con su mantilla tersiá. Permita Dios donde pongas todos tus cinco sentíos que paguen a tu querer como tú has pagao er mío. Quien te llamó Petenera no ha sabido darte nombre, que te debió poner, niña de mi corazón, perdición, ¡ay! de los hombres. Señor alcalde mayor, no prenda usté a los ladrones, que tiene usté una hija, niña de mi corazón, que roba los corazones. Valor, mare, que me matan; que no me puedo valer; son dos negros asesinos los ojos de esta muger."
 #input_song = "Déjame memoria triste; no me estás atormentando; se la quise o no la quise, niña de mi corazón, no me estás recordando."
 #input_song = "Y al aura el gentil capullo busca al imán el acero la fuente el que tiene sed y mi corazón al tuyo Y con esto me despido adiós público del alma al que le guste mi canto mil gracias da la gitana,"
-input_song = "Estaba la pájara pinta sentadita en el verde limón, con el pico recoge la hoja, con las alas recoge la flor. ¡Ay, sí! ¿Cuándo la veré yo? ¡Ay, sí! ¿Cuándo la veré yo? Me arrodillo a los pies de mi madre, fiel y constante, dame una mano, dame la otra, dame un besito que sea de tu boca."
+#input_song = "Estaba la pájara pinta sentadita en el verde limón, con el pico recoge la hoja, con las alas recoge la flor. ¡Ay, sí! ¿Cuándo la veré yo? ¡Ay, sí! ¿Cuándo la veré yo? Me arrodillo a los pies de mi madre, fiel y constante, dame una mano, dame la otra, dame un besito que sea de tu boca."
+#input_song = "Duérmete, mi niño con todo y tambache, tu madre la zorra, tu padre el tlacuache. Duérmete, niñita, que ahí viene el viejo, a llevarte viene con todo y pellejo. Duérmete, niñito, que ahí viene el coyote, a llevarte viene y a comerte al monte. Duérmete, mi niño, que estás en cajón; tu madre la zorra, tu padre el tejón. Duérmete, niñito, no venga el caucón, te quite la vida y a mí el corazón."
+input_song = "Me dices que era un gato el que entró por tu ventana. En la vida he visto yo, serrano de mi querer, un gato negro y con sotana."
 final_prompt = few_shot_prompt.format(input=input_song)
 
 
 
 
-print("FINAL PROMPT:")
-print(final_prompt)
+# print("FINAL PROMPT:")
+# print(final_prompt)
 
-output = llm.invoke(final_prompt)
+# output = llm.invoke(final_prompt)
 
-print("Output: " + output)
+# print("Output: " + output)
+
+
+for x in range (10):
+    output = llm.invoke(final_prompt)
+
+    print("Output " + str(x) + ": " + output)
 
 # match = re.search(r'\{.*\}', output)
 
